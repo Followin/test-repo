58b3b28b3a085dcc66776df5ceec973922c2c901
Following <Dlike.version10@gmail.com>
2015-09-02 07:09:13 +0300

    Load in
    Load

---
+++ test-repo/test-repo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(test_repo.Startup))]
+namespace test_repo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
